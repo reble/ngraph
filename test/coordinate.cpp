3667d9916af6669706810755717d304f85d990d6
Robert Kimball <robert.kimball@intel.com>
2018-10-01 07:40:48 -0700

    wip

--- test/coordinate.cpp
+++ test/coordinate.cpp
@@ -79,18 +79,133 @@ TEST(coordinate, shape3d)
 //                     const CoordinateDiff& target_padding_above,
 //                     const Strides& source_dilation_strides);
 
-TEST(coordinate, default)
+TEST(coordinate, corner)
 {
     Shape source_shape{10, 10};
-    Coordinate source_start_corner = Coordinate(source_shape.size(), 0);
+    Coordinate source_start_corner = Coordinate{0, 0};
     Coordinate source_end_corner{source_shape};
-    Strides source_strides;
-    AxisVector source_axis_order;
-    CoordinateDiff target_padding_below;
-    CoordinateDiff target_padding_above;
-    Strides source_dilation_strides;
+    Strides source_strides = Strides(source_shape.size(), 1);
+    AxisVector source_axis_order(source_shape.size());
+    iota(source_axis_order.begin(), source_axis_order.end(), 0);
+    CoordinateDiff target_padding_below = CoordinateDiff(source_shape.size(), 0);
+    CoordinateDiff target_padding_above = CoordinateDiff(source_shape.size(), 0);
+    Strides source_dilation_strides = Strides(source_shape.size(), 1);
 
-    auto ct = CoordinateTransform(source_shape, source_start_corner, source_end_corner);
+    auto ct = CoordinateTransform(source_shape,
+                                  source_start_corner,
+                                  source_end_corner,
+                                  source_strides,
+                                  source_axis_order,
+                                  target_padding_below,
+                                  target_padding_above,
+                                  source_dilation_strides);
+    for (const Coordinate& c : ct)
+    {
+        cout << c << endl;
+    }
+}
+
+TEST(coordinate, strides)
+{
+    Shape source_shape{10, 10};
+    Coordinate source_start_corner = Coordinate{0, 0};
+    Coordinate source_end_corner{source_shape};
+    Strides source_strides = Strides(source_shape.size(), 1);
+    AxisVector source_axis_order(source_shape.size());
+    iota(source_axis_order.begin(), source_axis_order.end(), 0);
+    CoordinateDiff target_padding_below = CoordinateDiff(source_shape.size(), 0);
+    CoordinateDiff target_padding_above = CoordinateDiff(source_shape.size(), 0);
+    Strides source_dilation_strides = Strides(source_shape.size(), 1);
+
+    auto ct = CoordinateTransform(source_shape,
+                                  source_start_corner,
+                                  source_end_corner,
+                                  source_strides,
+                                  source_axis_order,
+                                  target_padding_below,
+                                  target_padding_above,
+                                  source_dilation_strides);
+    for (const Coordinate& c : ct)
+    {
+        cout << ct.to_source_coordinate(c) << endl;
+    }
+}
+
+TEST(coordinate, axis_order)
+{
+    Shape source_shape{10, 10};
+    Coordinate source_start_corner = Coordinate{0, 0};
+    Coordinate source_end_corner{source_shape};
+    Strides source_strides = Strides(source_shape.size(), 1);
+    AxisVector source_axis_order(source_shape.size());
+    iota(source_axis_order.begin(), source_axis_order.end(), 0);
+    CoordinateDiff target_padding_below = CoordinateDiff(source_shape.size(), 0);
+    CoordinateDiff target_padding_above = CoordinateDiff(source_shape.size(), 0);
+    Strides source_dilation_strides = Strides(source_shape.size(), 1);
+
+    auto ct = CoordinateTransform(source_shape,
+                                  source_start_corner,
+                                  source_end_corner,
+                                  source_strides,
+                                  source_axis_order,
+                                  target_padding_below,
+                                  target_padding_above,
+                                  source_dilation_strides);
+
+    for (const Coordinate& c : ct)
+    {
+        cout << c << endl;
+    }
+}
+
+TEST(coordinate, padding)
+{
+    Shape source_shape{10, 10};
+    Coordinate source_start_corner = Coordinate{0, 0};
+    Coordinate source_end_corner{source_shape};
+    Strides source_strides = Strides(source_shape.size(), 1);
+    AxisVector source_axis_order(source_shape.size());
+    iota(source_axis_order.begin(), source_axis_order.end(), 0);
+    CoordinateDiff target_padding_below = CoordinateDiff(source_shape.size(), 0);
+    CoordinateDiff target_padding_above = CoordinateDiff(source_shape.size(), 0);
+    Strides source_dilation_strides = Strides(source_shape.size(), 1);
+
+    auto ct = CoordinateTransform(source_shape,
+                                  source_start_corner,
+                                  source_end_corner,
+                                  source_strides,
+                                  source_axis_order,
+                                  target_padding_below,
+                                  target_padding_above,
+                                  source_dilation_strides);
+
+    for (const Coordinate& c : ct)
+    {
+        cout << c << endl;
+    }
+}
+
+TEST(coordinate, dilation)
+{
+    Shape source_shape{10, 10};
+    Coordinate source_start_corner = Coordinate{0, 0};
+    Coordinate source_end_corner{source_shape};
+    Strides source_strides = Strides(source_shape.size(), 1);
+    AxisVector source_axis_order(source_shape.size());
+    iota(source_axis_order.begin(), source_axis_order.end(), 0);
+    CoordinateDiff target_padding_below = CoordinateDiff(source_shape.size(), 0);
+    CoordinateDiff target_padding_above = CoordinateDiff(source_shape.size(), 0);
+    Strides source_dilation_strides = Strides(source_shape.size(), 1);
+
+    auto ct = CoordinateTransform(source_shape,
+                                  source_start_corner,
+                                  source_end_corner,
+                                  source_strides,
+                                  source_axis_order,
+                                  target_padding_below,
+                                  target_padding_above,
+                                  source_dilation_strides);
+
     for (const Coordinate& c : ct)
     {
         cout << c << endl;
